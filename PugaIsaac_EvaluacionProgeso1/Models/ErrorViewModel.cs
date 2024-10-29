afeca88775444c21c06401bd2e34f86f1db5a744
Isaaidk <isaacpuga661@gmail.com>
2024-10-29 07:22:54 -0500

    Agregar archivos de proyecto.

---
+++ PugaIsaac_EvaluacionProgeso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PugaIsaac_EvaluacionProgeso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
